4cc102e07d84ec4d877eb8cb495954f8c9bfd2d8
carferrio <153732077+carferrio@users.noreply.github.com>
2024-10-01 18:53:54 +0100

    Frameworks and Libraries updated

--- libs/php/getEmployeesByDepartment.php
+++ libs/php/getEmployeesByDepartment.php
@@ -1,11 +1,6 @@
 <?php
 
-	// example use from browser
-	// use insertDepartment.php first to create new dummy record and then specify it's id in the command below
-	// http://localhost/companydirectory/libs/php/deleteDepartmentByID.php?id=<id>
 
-	// remove next two lines for production
-	
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 
@@ -18,7 +13,7 @@
 	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
 
 	if (mysqli_connect_errno()) {
-		
+
 		$output['status']['code'] = "300";
 		$output['status']['name'] = "failure";
 		$output['status']['description'] = "database unavailable";
@@ -31,40 +26,51 @@
 
 		exit;
 
-	}	
+	}
 
-	// SQL statement accepts parameters and so is prepared to avoid SQL injection.
-	// $_REQUEST used for development / debugging. Remember to change to $_POST for production
+	// first query - SQL statement accepts parameters and so is prepared to avoid SQL injection.
+	// $_POST used for development / debugging. Remember to change to $_POST for production
 
-	$query = $conn->prepare('DELETE FROM department WHERE id = ?');
-	
+	$query = $conn->prepare('SELECT count(p.id) as departmentCount, d.name as departmentName FROM personnel p LEFT JOIN department d ON ( d.id = p.departmentID) WHERE d.id = ?');
+
 	$query->bind_param("i", $_REQUEST['id']);
 
 	$query->execute();
-	
+
 	if (false === $query) {
 
 		$output['status']['code'] = "400";
 		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
+		$output['status']['description'] = "query failed";
 		$output['data'] = [];
 
 		mysqli_close($conn);
 
-		echo json_encode($output); 
+		echo json_encode($output);
 
 		exit;
 
 	}
 
+	$result = $query->get_result();
+
+   	$department = [];
+
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		array_push($department, $row);
+
+	}
+
+
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = [];
-	
+	$output['data']['department'] = $department;
+
 	mysqli_close($conn);
 
-	echo json_encode($output); 
+	echo json_encode($output);
 
 ?>
